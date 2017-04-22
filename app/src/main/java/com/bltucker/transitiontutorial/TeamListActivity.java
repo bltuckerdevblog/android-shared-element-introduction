6df1341b76a7daf48152395cbaeb8f0b4fc4545c
Brett <bretttucker@gmail.com>
2017-04-22 17:33:18 -0600

    Add initial project files

---
+++ app/src/main/java/com/bltucker/transitiontutorial/TeamListActivity.java
@@ -0,0 +1,15 @@
+package com.bltucker.transitiontutorial;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TeamListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_list);
+    }
+}
